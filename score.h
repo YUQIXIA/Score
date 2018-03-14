e48ac7a3c9af7930969b7c9ff837e49b05f0a78b
YUQIXIA <491674566@qq.com>
2018-03-14 08:42:46 +0000

    third commit

---
+++ score.h
@@ -0,0 +1,8 @@
+//constant define
+#define MAX 30
+
+//fiunciton prototypes
+void input_scores(int []);
+void evaluation(const int[], int[]);
+void classification(const int[],int[]);
+void output_results(const int[],const int[]);
